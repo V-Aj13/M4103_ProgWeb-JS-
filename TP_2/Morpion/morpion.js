6d4fc7aa588921ca7b788269ff7332a9da1c65de
V-Aj13 <aldous.villalobos@edu.univ-paris13.fr>
2022-02-14 15:23:31 +0100

    refactor morpion.js

--- TP_2/Morpion/morpion.js
+++ TP_2/Morpion/morpion.js
@@ -2,10 +2,16 @@ console.log("Ce programme JS vient d'être chargé");
 $(document).ready(function()
 {
 	console.log("Le document est pret");
+	var joueur="X";
+
 	$('#morpion td').mousedown(function()
 		{
 			console.log("Le bouton de la souris a été enfoncé.");
-			$(this).text('X');
+			if($(this).text()!==''){return;}
+			$(this).text(joueur);
+			joueur=(joueur==="X" ? 'O' : 'X');
 		});
+
 	console.log("La mise en place est finie. En attente d'événements...");
 });
+
